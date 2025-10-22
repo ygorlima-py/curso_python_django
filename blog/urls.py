d9ede6591253883a02e609ce6bdcb8f12c212a16
Ygor Lima <ygor.limarsx@gmail.com>
2025-10-22 08:17:41 -0300

    lançando erro 404 e ajustando as URLs estáticas para clicar e abrir o post

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from .views import blog, exemplo, post
 app_name = 'blog'
 
 urlpatterns = [
+    path('post/<int:post_id>/', post, name='post'),
     path('', blog, name='home'),
-    path('post/<int:id>', post, name='post'),
     path('exemplo/', exemplo, name='exemplo')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 import requests 
-import json
+from django.http import HttpRequest, Http404
 
 
 
 # Create your views here.
 
 # Página Home
-def blog(request):
+def blog(request: HttpRequest):
     posts = requests.get('https://jsonplaceholder.typicode.com/posts')
     posts_json = posts.json()
 
@@ -23,7 +23,9 @@ def blog(request):
     )
 
 # Pagina Exemplo
-def exemplo(request):
+def exemplo(request: HttpRequest):
+
+    print (request)
 
     context = dict(
         text='Exemplo da página Blog',
@@ -36,18 +38,32 @@ def exemplo(request):
         context,
     )
 
-def post(request, id):
+def post(request: HttpRequest, post_id: int):
+
     posts = requests.get('https://jsonplaceholder.typicode.com/posts')
     posts_json = posts.json()
 
+    found_post = None
+
+    for post in posts_json:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Http404('Post Não existe')
+        # Aqui adicionamo uma excessão, erro 404, significa quando o servidor não encontrou a pagina solicitada
+        # Outro ponto, sem essa excessão ele lança erro 500, que não é vdd, pois o erro é do cliente
+    
     context = dict(
         # text='Meu Blog',
-        posts=posts_json,
+        post=found_post,
+        title=found_post['title'],
     )
 
     return render(
         request, 
-        'blog/index.html',
+        'blog/post.html',
         context=context,
     )
 
